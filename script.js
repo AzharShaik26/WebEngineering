8267a9bdb3dc234ace6f99ffb2f7e40d02d19bd6
AzharShaik26 <101628517+AzharShaik26@users.noreply.github.com>
2022-03-15 14:04:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let arr =["Cvr college","12537",["Mangalpally","Ibp"]]
+console.log(arr[0])
+let [clg,num,[address]]=arr
+console.log(clg,num,address)
+let name={
+    f_name: "deva"
+}
+console.log(name.f_name)
