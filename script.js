22956eb6e96596a504c56b4c24d1d46c7ebacd7d
AzharShaik26 <101628517+AzharShaik26@users.noreply.github.com>
2022-03-15 14:06:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log(arr[0])
 let [clg,num,[address]]=arr
 console.log(clg,num,address)
 let name={
-    f_name: "deva"
+    f_name: "Git"
+    l_name:"Hub"
 }
-console.log(name.f_name)
+console.log(name.f_name,name.l_name)
