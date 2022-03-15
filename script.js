2cca41ab387cd977a05b35bf2ddb43ca12bfd9ff
AzharShaik26 <101628517+AzharShaik26@users.noreply.github.com>
2022-03-15 14:24:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let arr =["Cvr college","12537",["Mangalpally","Ibp"]]
-console.log(arr[0])
-let [clg,num,[address]]=arr
-console.log(clg,num,address)
-let name={
-    f_name: "Git"
-    l_name:"Hub"
-}
-console.log(name.f_name,name.l_name)
